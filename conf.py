7dc7b017a76ba2b9f71c90c6c1f3760dbc6f2676
nazarov <collermix@gmail.com>
2023-07-27 19:11:29 +0600

    Add AMPLITUDE_API_KEY

--- conf.py
+++ conf.py
@@ -11,3 +11,4 @@ OPEN_API_TOKEN = config["OPEN_API_TOKEN"]
 WEB_APP_HOST = config['web_app_host']
 WEB_APP_PORT = config['web_app_port']
 WEB_APP_URL = f"http://{config['web_app_host']}:{config['web_app_port']}"
+AMPLITUDE_API_KEY = config["AMPLITUDE_API_KEY"]
